5d759ad071eac7b2c60486b5f364dd60cbce1b3b
Kaynan-Jamelaumn <kaynanrodrigues.nt@gmail.com>
2023-08-17 15:03:50 -0300

    initial

---
+++ project/website/views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template, url_for
+
+views = Blueprint('views', __name__) # views é o que é importado no init
+
+@views.route('/')
+def home():
+    return render_template("index.html")
