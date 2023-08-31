ad2cdded6f60ba6c8dbcefc965e055ffe1bced50
Kaynan-Jamelaumn <kaynanrodrigues.nt@gmail.com>
2023-08-30 21:21:34 -0300

    howfix

--- project/website/models.py
+++ project/website/models.py
@@ -9,12 +9,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     notes = db.relationship('Note')
-    
+
+    def __repr__(self):
+        return f'{self.id} {self.email} {self.name}'
+
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1000))
-    data = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #user_id is the name of the table and the field
-
-        
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # user_id is the name of the table and the field
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    public = db.Column(db.Boolean, default=False)
